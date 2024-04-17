d04abbd6d54b1edf6f4395777e5406e0e7e7e7aa
bhavesh <jainbhavesh2707@gmail.com>
2024-04-17 13:25:47 +0530

    add

--- Terraform/module/Lambda/main.tf
+++ Terraform/module/Lambda/main.tf
@@ -11,6 +11,12 @@ resource "aws_lambda_function" "api_function" {
 
   role = var.iam_role
 }
+resource "aws_lambda_permission" "allow_api" {
+  statement_id  = "AllowAPIgatewayInvokation"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api_function.invoke_arn
+  principal     = "apigateway.amazonaws.com"
+}
 
 output "invoke_arn" {
   value = aws_lambda_function.api_function.invoke_arn
